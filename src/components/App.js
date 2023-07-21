e60d5b3bbb1da35f142c192887dcaf520a49702e
sachinsherekar <130824306+sachinsherekar@users.noreply.github.com>
2023-07-21 07:36:26 +0000

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[greet,setGreet]=useState();
+  const handleChange=(event)=>{
+    console.log(event.target.value);
+    setGreet(event.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        Enter your name: 
+        <br/>
+        <input type="text" onChange={handleChange}/>
+        <p>{greet && `Hello ${greet}!`}</p>
     </div>
   )
 }
